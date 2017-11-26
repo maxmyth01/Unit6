8a6bb322ee00f3f3108ab21a27dc0ab1fde0a0e0
smeds1 <smeds1@users.noreply.github.com>
2017-11-26 13:17:30 -0500

    Updated from Brython Server: 11/26/2017 1:17:30 PM

--- longestDictionaryWord.py
+++ longestDictionaryWord.py
@@ -1,1 +1,15 @@
+#Sam Smedinghoff
+#11/26/17
+#reverseFile.py - reverses the order of the lines in a file
 
+fileName = input('What file would you like to open? ')
+
+file = open(fileName)
+
+lines = []
+for line in file:
+    lines.append(line)
+lines.reverse()
+
+for line in lines:
+    print(line)
