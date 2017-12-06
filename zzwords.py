eeb3402d4c80cceda19931133f4c1335befd4b86
maxmyth01 <maxmyth01@gmail.com>
2017-12-06 13:54:08 -0500

    Updated from Brython Server: 12/6/2017 1:54:08 PM

--- zzwords.py
+++ zzwords.py
@@ -1,1 +1,14 @@
+#Max Low
+#12-6-17
+#zzwords.py -- search ditionary for word containing zz
+
+dictionary = open('engmix.txt')
+
+wordCount = 0
+for word in dictionary:
+    if 'zz' in word:
+        print(word.strip())
+    wordCount += 1
+    
+print('There are', wordCount,'words in the dictionary')
 
