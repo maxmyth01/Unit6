216acbbd4ddd8d7591a9da0538fbbe20e5fa92ff
maxmyth01 <maxmyth01@gmail.com>
2017-12-06 13:50:49 -0500

    Updated from Brython Server: 12/6/2017 1:50:49 PM

--- longestDictionaryWord.py
+++ longestDictionaryWord.py
@@ -1,1 +1,13 @@
+#Max Low
+#12-6-17
+#longestDictionaryWord.py
 
+dictionary = open('engmix.txt')
+
+wordSize = 0
+for word in dictionary:
+    if len(word) > wordsize:
+        wordsize = len(word) 
+for word in dictionary:
+    if len(word) = wordsize:
+        print(word)
