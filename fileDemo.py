39004a902e869bd8c1a4ec6828e653ef7498686f
maxmyth01 <maxmyth01@gmail.com>
2017-12-06 13:43:10 -0500

    Updated from Brython Server: 12/6/2017 1:43:10 PM

--- fileDemo.py
+++ fileDemo.py
@@ -6,6 +6,8 @@ dictionary = open('engmix.txt')
 
 wordCount = 0
 for word in dictionary:
+    if 'smed' in word:
+        print(word)
     wordCount += 1
     
 print('There are', wordCount,'words in the dictionary')
