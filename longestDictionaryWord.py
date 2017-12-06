9926c57f6d390ca30a8208af31cdcd71c3259b9b
maxmyth01 <maxmyth01@gmail.com>
2017-12-06 13:51:09 -0500

    Updated from Brython Server: 12/6/2017 1:51:09 PM

--- longestDictionaryWord.py
+++ longestDictionaryWord.py
@@ -9,5 +9,5 @@ for word in dictionary:
     if len(word) > wordsize:
         wordsize = len(word) 
 for word in dictionary:
-    if len(word) = wordsize:
+    if wordsize == len(word):
         print(word)
