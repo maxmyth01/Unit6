d25d9f63ebf80c654fab85c136704109af1604bd
maxmyth01 <maxmyth01@gmail.com>
2017-12-06 13:59:16 -0500

    Updated from Brython Server: 12/6/2017 1:59:16 PM

--- howManyWords.py
+++ howManyWords.py
@@ -1,1 +1,12 @@
+#Max Low
+#12-6-17
+#howManyWords.py -- prints out how many words for number of letters
+
+dictionary = open('engmix.txt')
+
+list = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+
+for word in dictionary:
+    list[len(word)] += 1
+
 
