c6f059c48b51a4423fc38653ed7441b0e33ad9d1
maxmyth01 <maxmyth01@gmail.com>
2017-12-06 13:42:16 -0500

    Updated from Brython Server: 12/6/2017 1:42:16 PM

--- fileDemo.py
+++ fileDemo.py
@@ -2,4 +2,11 @@
 #12-6-17
 #fileDemo.py
 
-open('engmix.txt')
+dictionary = open('engmix.txt')
+
+wordCount = 0
+for word in dictionary:
+    wordCount += 1
+    
+print('There are', wordCount,'words in the dictionary')
+
