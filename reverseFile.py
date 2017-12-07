45e730b0332584bb97d47578654a94a00bfa92d0
maxmyth01 <maxmyth01@gmail.com>
2017-12-07 14:36:42 -0500

    Updated from Brython Server: 12/7/2017 2:36:42 PM

--- reverseFile.py
+++ reverseFile.py
@@ -1,8 +1,12 @@
 #Max Low
 #12/7/17
 #reverseFile.py -- askes user for file then prints out all lines in reverse
-
 file = open(input('Enter a file name:'))
-
 for line in file:
-    print(line)
+    next = -1
+    for ch in line:
+        newline = []
+        ch -= 1
+        next += 1 
+        newline[next] = line[ch]
+    print(newline)
