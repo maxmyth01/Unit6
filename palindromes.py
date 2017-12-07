3703ed7bc7f8839c86baa36ac75a6751f1b5a4e6
maxmyth01 <maxmyth01@gmail.com>
2017-12-07 14:50:21 -0500

    Updated from Brython Server: 12/7/2017 2:50:20 PM

--- palindromes.py
+++ palindromes.py
@@ -1,1 +1,9 @@
+#Max Low
+#12-7-17
+#palindromesall the same forwards as a backwords in dictionary
 
+dictionary = open('engmix.txt')
+
+for word in dictionary:
+    if word == word.reverse():
+        print(word)
