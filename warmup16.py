55338e5601e5af4c511d261bf6936f5001710962
maxmyth01 <maxmyth01@gmail.com>
2017-12-11 14:22:10 -0500

    Updated from Brython Server: 12/11/2017 2:22:09 PM

--- warmup16.py
+++ warmup16.py
@@ -1,1 +1,13 @@
+#Max low
+#12-11-17
+#warmup16.py -- words in dictionary start with M and end w/L
 
+dictionary = open('engmix.txt')
+
+Mlist = []
+for line in dictionary:
+    if line.strip()[0] == 'M'
+        Mlist.append()
+        for word in Mlist:
+            if word[len(word)-1] == "L":
+                print(word)
