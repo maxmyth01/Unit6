b9ba47edf362291d2a20a8c64ba5a605e8996162
maxmyth01 <maxmyth01@gmail.com>
2017-12-13 13:27:41 -0500

    Updated from Brython Server: 12/13/2017 1:27:39 PM

--- quiz6.py
+++ quiz6.py
@@ -1,1 +1,15 @@
+#Max Low
+#12-13-17
+#quiz6.py -- search dictionary for words with 3c's and 2p's -- words that start with r -- for a inputed number print the first word with that many letters -- ask user for letter print out all word w/o that letter -- load dictionary into list print the middle
 
+dictionary = open("engmix.txt")
+    for word in dictionary:
+        c=0
+        p=0
+        for letter in word:
+            if letter == "c":
+                c+=1
+            elif letter == "p":
+                p+=1
+        if c == 3 and p == 2:
+            print(word)
